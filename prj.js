497bf54222f23f4493c6a4906b556672805213e5
minhzuuu <minhlon127@gmail.com>
2023-10-31 09:11:33 +0700

    Add prj.js

---
+++ prj.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('.nav');
+const navOpen = document.querySelector('.menu-ic');
+
+
+navOpen.addEventListener('click', function()
+{
+    menu.classList.toggle('showmenu')
+})
+
